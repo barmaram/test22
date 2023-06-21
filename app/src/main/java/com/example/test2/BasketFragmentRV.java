8b96539a5b6f3b4dc15045315f2eb8b93161fba5
maram <barbourmaram@gmail.com>
2023-06-22 01:24:41 +0300

    offf

--- app/src/main/java/com/example/test2/BasketFragmentRV.java
+++ app/src/main/java/com/example/test2/BasketFragmentRV.java
@@ -4,12 +4,14 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -34,6 +36,7 @@ public class BasketFragmentRV extends Fragment {
     FirebaseServices FBS;
     User user;
     ArrayList<String> ClothePathArrayList , finalPath;
+    Button btn ;
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -95,6 +98,13 @@ public class BasketFragmentRV extends Fragment {
                     }
                 });
         recyclerView = getView().findViewById(R.id.BasketRV);
+        btn=getView().findViewById(R.id.BasketBuyBtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                BasketTrans();
+            }
+        });
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
         FBS= FirebaseServices.getInstance();
@@ -129,7 +139,11 @@ public class BasketFragmentRV extends Fragment {
         }
     }
 
-
+      public void BasketTrans() {
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.FlMain, new CardFragment());
+        ft.commit();
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/test2/CardFragment.java
@@ -0,0 +1,129 @@
+package com.example.test2;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.text.InputType;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.braintreepayments.cardform.view.CardForm;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CardFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CardFragment extends Fragment {
+  Button Buy;
+  CardForm cardForm;
+  AlertDialog.Builder alertBuilder;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public CardFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CardFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static CardFragment newInstance(String param1, String param2) {
+        CardFragment fragment = new CardFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_card, container, false);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        connectComponents();
+    }
+
+
+    private void connectComponents() {
+        Buy=getView().findViewById(R.id.btnBuy);
+        cardForm=getView().findViewById(R.id.cardform);
+        cardForm.cardRequired(true)
+                .expirationRequired(true)
+                .cvvRequired(true)
+                .cardholderName(CardForm.FIELD_REQUIRED)
+                .postalCodeRequired(true)
+                .mobileNumberRequired(true)
+                .mobileNumberExplanation("SMS is required on this number")
+                .actionLabel("Purchase")
+                .setup(getActivity());
+        cardForm.getCvvEditText().setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        Buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (cardForm.isValid()) {
+                    AlertDialog.Builder alertBuilder = new AlertDialog.Builder(getActivity());
+                    alertBuilder.setTitle("Confirm before purchase");
+                    alertBuilder.setMessage("Card number: " + cardForm.getCardNumber() + "\n" +
+                            "Card expiry date: " + cardForm.getExpirationDateEditText().getText().toString() + "\n" +
+                            "Card CVV: " + cardForm.getCvv() + "\n" +
+                            "Postal code: " + cardForm.getPostalCode() + "\n" +
+                            "Phone number: " + cardForm.getMobileNumber());
+                    alertBuilder.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            Toast.makeText(getActivity(), "Thank you for purchase", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                    alertBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                        }
+                    });
+                    AlertDialog alertDialog = alertBuilder.create();
+                    alertDialog.show();
+
+                } else {
+                    Toast.makeText(getActivity(), "Please complete the form", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
